2e5ad386de83b5ba5cd4e5f37edcd473689a6db7
User <user@example.com>
2025-10-21 20:32:16 +0800

    多余的右花括号·······

--- vite.config.ts
+++ vite.config.ts
@@ -32,5 +32,15 @@ export default defineConfig({
     __VUE_OPTIONS_API__: false,
     __VUE_PROD_DEVTOOLS__: false,
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
   }
 })
